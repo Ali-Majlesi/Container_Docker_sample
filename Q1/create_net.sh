c87e9a43b20b55b62efe8a814d43681d50a7081d
Ali Majlesi <comcpum@gmail.com>
2023-06-05 17:06:42 +0330

    create_net Q1 bugs fixed

--- Q1/create_net.sh
+++ Q1/create_net.sh
@@ -38,25 +38,8 @@ ip netns exec node4 ip address add 10.10.0.3/24 dev eth0-node4
 ip netns exec router ip address add 172.0.0.1/24 dev eth1
 ip netns exec router ip address add 10.10.0.1/24 dev eth2
 
-# Add default gateway
-ip netns exec node1 ip route add default via 172.0.0.1
-ip netns exec node2 ip route add default via 172.0.0.1
-ip netns exec node3 ip route add default via 10.10.0.1
-ip netns exec node4 ip route add default via 10.10.0.1
 
-# Assign ip to nodes
-ip netns exec node1 ip address add 172.0.0.2/24 dev eth0-node1
-ip netns exec node2 ip address add 172.0.0.3/24 dev eth0-node2
-ip netns exec node3 ip address add 10.10.0.2/24 dev eth0-node3
-ip netns exec node4 ip address add 10.10.0.3/24 dev eth0-node4
-ip netns exec router ip address add 172.0.0.1/24 dev eth1
-ip netns exec router ip address add 10.10.0.1/24 dev eth2
 
-# Add default gateway
-ip netns exec node1 ip route add default via 172.0.0.1
-ip netns exec node2 ip route add default via 172.0.0.1
-ip netns exec node3 ip route add default via 10.10.0.1
-ip netns exec node4 ip route add default via 10.10.0.1
 
 # Setting links in the root namespace up
 ip link set veth-1 up
@@ -80,7 +63,11 @@ ip netns exec node4 ip link set dev eth0-node4 up
 ip netns exec router ip link set dev eth1 up
 ip netns exec router ip link set dev eth2 up
 
-
+# Add default gateway
+ip netns exec node1 ip route add default via 172.0.0.1
+ip netns exec node2 ip route add default via 172.0.0.1
+ip netns exec node3 ip route add default via 10.10.0.1
+ip netns exec node4 ip route add default via 10.10.0.1
 
 ip netns exec node1 ip route
 
